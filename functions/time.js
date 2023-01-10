ad8fb4b34aafce9d984e74dba6d4cce60069b06b
Ted Staggs <72319119+tedstaggs01@users.noreply.github.com>
2023-01-10 17:01:23 +0100

    gg

--- functions/time.js
+++ functions/time.js
@@ -1,4 +1,6 @@
 function getutc() {
     const date = new Date();
+    console.log(date.getUTCHours());
+    console.log(date.getUTCMinutes());
     document.getElementById("utc").innerHTML = date.getUTCHours() + ":"+  date.getUTCMinutes();
 }
