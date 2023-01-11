f080b6cbc1781069b7a3527cf2b7616a4d2e52d4
Ted Staggs <72319119+tedstaggs01@users.noreply.github.com>
2023-01-11 23:59:52 +0100

    sd

--- functions/wb.js
+++ functions/wb.js
@@ -1,59 +1,61 @@
 
 
-function calculatewb(){
-              // empty weight
-var emptyweightW = document.getElementById("emptyweightW").innerHTML;
-var emptyweightA = 1912;
-var emptyweightM = emptyweightW * emptyweightA;
-
-// Station cockpit
-
-var crewW = document.getElementById("crewW").innerHTML;
-var crewA = 965;
-var crewM = crewW * crewA;
-
-// Station PAX
-
-var paxW = document.getElementById("paxW").innerHTML;
-var paxA = 1514;
-var paxmom = paxW * paxA;
-
-// Station Baggage
-
-var baggageW = document.getElementById("baggageW").innerHTML;
-var baggageA = 1514;
-var baggageM = baggageW * baggageA;
-
-// Station Fuel
-
-var fuelW = document.getElementById("fuelW").innerHTML;
-var fuelA = 1715;
-var fuelM = fuelW * fuelA;
-
-// startup; taxi; runup fuel
-
-var taxiW = document.getElementById("taxiW").innerHTML;
-
-// Trip Fuel
-
-var tripW = document.getElementById("tripW").innerHTML; 
-
-// weights
-
-var zerofuelW = emptyweightW + crewW + paxW + baggageW;;
-var rampW = zerofuelW + fuelW;
-var takeoffW = rampW - taxiW;
-var landingW = takeoffW - tripW;
-
-// moments
-
-var zerofuelM = zerofuelA * zerofuelW;
-var rampM = rampA * rampW;
-var takeoffM = takeoffW * takeoffA;
-var landingM = landingA * landingW;
-
-    // Centre of gravities
-
+function wbcalculate(){
+    
+    // Weights
+    
+    var emptyweightW = document.getElementById("emptyweightW").innerHTML;
+    var emptyweightA = 1912;
+    var emptyweightM = emptyweightW * emptyweightA;
+    
+    // Station cockpit
+    
+    var crewW = document.getElementById("crewW").innerHTML;
+    var crewA = 965;
+    var crewM = crewW * crewA;
+    
+    // Station PAX
+    
+    var paxW = document.getElementById("paxW").innerHTML;
+    var paxA = 1514;
+    var paxmom = paxW * paxA;
+    
+    // Station Baggage
+    
+    var baggageW = document.getElementById("baggageW").innerHTML;
+    var baggageA = 1514;
+    var baggageM = baggageW * baggageA;
+    
+    // Station Fuel
+    
+    var fuelW = document.getElementById("fuelW").innerHTML;
+    var fuelA = 1715;
+    var fuelM = fuelW * fuelA;
+    
+    // startup; taxi; runup fuel
+    
+    var taxiW = document.getElementById("taxiW").innerHTML;
+    
+    // Trip Fuel
+    
+    var tripW = document.getElementById("tripW").innerHTML; 
+    
+    // weights
+    
+    var zerofuelW = emptyweightW + crewW + paxW + baggageW;;
+    var rampW = zerofuelW + fuelW;
+    var takeoffW = rampW - taxiW;
+    var landingW = takeoffW - tripW;
+    
+    // moments
+    
+    var zerofuelM = zerofuelA * zerofuelW;
+    var rampM = rampA * rampW;
+    var takeoffM = takeoffW * takeoffA;
+    var landingM = landingA * landingW;
+    
+        // Centre of gravities
+    
     var zerof 
     var rampA = rampM / rampW;
     var takeoffA = takeoffM / takeoffW;
@@ -64,7 +66,7 @@ var landingM = landingA * landingW;
     document.getElementById("ldgW").innerHTML = landingW + " kg";
 }
 
-calculatewb();
+wbcalculate();
 
 // these values will be transferred into a graph
 // pdf output upon button 
