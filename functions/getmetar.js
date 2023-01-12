0fefd793790bc4883c5c608461a032825b8b47d1
Ted Staggs <72319119+tedstaggs01@users.noreply.github.com>
2023-01-13 00:01:15 +0100

    nh

--- functions/getmetar.js
+++ functions/getmetar.js
@@ -15,15 +15,15 @@ function success(response) {
       function quicklabel(){
         document.getElementById('quicklabel').innerText = metar.flight_category;
         var quickla = metar.flight_category;
-          if (quickla = "VFR") {
+        if (quickla = "VFR") {
           document.getElementById("quicklabel").style.background = "#04AA6D";
-          } if (quickla = "MVFR"){
+        } if (quickla = "MVFR"){
           document.getElementById("quicklabel").style.background = "#2196F3";
-          } if (quickla = "IFR") {
+        } if (quickla = "IFR") {
           document.getElementById("quicklabel").style.background = "#f44336";
-          } if (quickla = "LIFR") {
+        } if (quickla = "LIFR") {
           document.getElementById("quicklabel").style.background = "#b300b3";
-          }
+        }
       }
       quicklabel();
 
