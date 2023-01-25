995281cae07a61867ad4ddd1d801c5ea34efefe9
Ted Staggs <72319119+tedstaggs01@users.noreply.github.com>
2023-01-25 17:24:52 +0100

    sdfsf

--- functions/checklist.js
+++ functions/checklist.js
@@ -1,9 +1,78 @@
-function markchecklistitem() {
-    if (document.getElementById("check").checked == true) {
+function markchecklistitem1() {
+    if (document.getElementById("check1").checked == true) {
         
-        document.getElementById("checkvalue").style.color = "green";
+        document.getElementById("checkvalue1").style.color = "green";
         
     } else {
-        document.getElementById("checkvalue").style.color = "grey";
+        document.getElementById("checkvalue1").style.color = "grey";
     }
 }
+
+function markchecklistitem2() {
+    if (document.getElementById("check2").checked == true) {
+        
+        document.getElementById("checkvalue2").style.color = "green";
+        
+    } else {
+        document.getElementById("checkvalue2").style.color = "grey";
+    }
+}
+
+function markchecklistitem3() {
+    if (document.getElementById("check3").checked == true) {
+        
+        document.getElementById("checkvalue3").style.color = "green";
+        
+    } else {
+        document.getElementById("checkvalue3").style.color = "grey";
+    }
+}
+
+function markchecklistitem4() {
+    if (document.getElementById("check4").checked == true) {
+        
+        document.getElementById("checkvalue4").style.color = "green";
+        
+    } else {
+        document.getElementById("checkvalue4").style.color = "grey";
+    }
+}
+
+function markchecklistitem5() {
+    if (document.getElementById("check5").checked == true) {
+        
+        document.getElementById("checkvalue5").style.color = "green";
+        
+    } else {
+        document.getElementById("checkvalue5").style.color = "grey";
+    }
+}
+
+function markchecklistitem6() {
+    if (document.getElementById("check6").checked == true) {
+        
+        document.getElementById("checkvalue6").style.color = "green";
+        
+    } else {
+        document.getElementById("checkvalue6").style.color = "grey";
+    }
+}
+
+function markchecklistitem7() {
+    if (document.getElementById("check7").checked == true) {
+        
+        document.getElementById("checkvalue7").style.color = "green";
+        
+    } else {
+        document.getElementById("checkvalue7").style.color = "grey";
+    }
+}
+
+/*
+function markchecklistcomplete1() {
+    if (document.getElementById("check1").checked == true){
+        document.getElementById("").style.color = "green";
+    }
+}
+
+*/
