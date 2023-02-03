2ea0624562808837f4ad0f1add8d763c2d24084f
Ted Staggs <72319119+tedstaggs01@users.noreply.github.com>
2023-02-03 23:53:33 +0100

    sds

--- functions/fielopener.js
+++ functions/fielopener.js
@@ -1,17 +1,17 @@
 document.getElementById("file_1").addEventListener("click", function() {
-    window.open("path/to/file.pdf", "_blank");
+    window.open("/documents/C01-01-08-Amdt-5.pdf", "_blank");
 });
 
 document.getElementById("file_2").addEventListener("click", function() {
-    window.open("path/to/file.pdf", "_blank");
+    window.open("/documents/CASA Approved GA8 MMEL.pdf", "_blank");
 });
 
 document.getElementById("file_3").addEventListener("click", function() {
-    window.open("path/to/file.pdf", "_blank");
+    window.open("/documents/Suppl. Increased Takeoff Mass.pdf", "_blank");
 });
 
 document.getElementById("file_4").addEventListener("click", function() {
-    window.open("path/to/file.pdf", "_blank");
+    window.open("/documents/Parachute Supplement C01-01-01,    Amdt. 4.pdf", "_blank");
 });
 
 
