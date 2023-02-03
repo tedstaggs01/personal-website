37a3d411e4be01b9de45b0418b90bcee7134949c
Ted Staggs <72319119+tedstaggs01@users.noreply.github.com>
2023-02-03 23:48:48 +0100

    adsadda

---
+++ functions/fielopener.js
@@ -0,0 +1,20 @@
+document.getElementById("file_1").addEventListener("click", function() {
+    window.open("path/to/file.pdf", "_blank");
+});
+
+document.getElementById("file_2").addEventListener("click", function() {
+    window.open("path/to/file.pdf", "_blank");
+});
+
+document.getElementById("file_3").addEventListener("click", function() {
+    window.open("path/to/file.pdf", "_blank");
+});
+
+document.getElementById("file_4").addEventListener("click", function() {
+    window.open("path/to/file.pdf", "_blank");
+});
+
+
+
+
+  
