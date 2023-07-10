5346ed876f80691a7a7a86c3ead4ee5e376bc8ca
Ted Staggs <72319119+tedstaggs01@users.noreply.github.com>
2023-07-10 14:55:35 +0200

    s

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const app = express()
+const bcrypt = require("bcrypt")
+
+const users = []
 
 app.set("view-engine", "ejs")
+app.use(express.urlencoded({extended: false}))
 
 app.get("/", (req, res) => {
     res.render("calendar.ejs", {name: "Ted"})
@@ -13,6 +17,22 @@ app.get("/login", (req,res) => {
 
 app.get("/register", (req, res) => {
     res.render("register.ejs")
+})
+
+app.post("/register", (req, res) => {
+    try {
+        const hashedPassword = bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name, 
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect("/login")
+    } catch {
+        res.redirect("/register")
+    }
+    console.log(users)
 })
 
 app.listen(3000)
