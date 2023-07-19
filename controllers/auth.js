660c93388393fe17f054b4206ac3c5bae2de1aed
Ted Staggs <72319119+tedstaggs01@users.noreply.github.com>
2023-07-19 12:44:24 +0200

    d

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,19 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER, 
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
 exports.register = (req, res) => {
     console.log(req.body)
+
+    const { name, email, password } = req.body;
+
+    db.query('SELECT email FROM users')
+
+
     res.send('Form Submitted')
 }
